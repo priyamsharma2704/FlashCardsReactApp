acd8c7c8c3d0fb173d390982e7f1703f1d8488a0
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-10-07 15:10:11 -0400

    delete implemented

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -2,17 +2,17 @@ import {useState} from 'react';
 import EditImg from '../assets/pen-to-square-regular.svg';
 import DeleteImg from '../assets/trash-can-regular.svg';
 import CardModal from './CardModal';
-import { useModalStore } from '../Store/store';
+import { useModalStore, useCardListStore } from '../Store/store';
 function Card({data, index})
 {
     const [showAnswer, setShowAnswer] = useState(false);
-
     const [isInEditMode, setIsInEditMode] = useState(false);
+
+    const {cardsList, updateCardList} = useCardListStore();
     const {setQuestion, setAnswer, setId} = useModalStore();
 
     function handleShowAnswer()
     {
-        console.log("show/hide")
         setShowAnswer(!showAnswer);
     }
 
@@ -25,9 +25,11 @@ function Card({data, index})
         setIsInEditMode(!isInEditMode);
     }
 
-    function handleDelete()
+    function handleDelete(index)
     {
-        console.log("del")
+        let cardsListCopy = [...cardsList];
+        cardsListCopy.splice(index, 1);
+        updateCardList(cardsListCopy);
     }
 
     function closeModal()
@@ -43,7 +45,7 @@ function Card({data, index})
                     <button className="show-hide-btn" onClick={handleShowAnswer}>Show/Hide</button>
                     <div className="btns-con">
                         <img className='edit' src={EditImg} onClick={handleEdit}></img>
-                        <img className='delete' src={DeleteImg} onClick={handleDelete}/>
+                        <img className='delete' src={DeleteImg} onClick={()=>handleDelete(index)}/>
                     </div>
             </div>
             {isInEditMode && <CardModal index={index} cardId={data.id} closeModal={closeModal}></CardModal>}
