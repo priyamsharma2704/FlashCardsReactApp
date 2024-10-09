65e6dc4fcadc83c37afbf9e992b834586412b937
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-10-09 10:49:16 -0400

    displying the cards list after fetching it from db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
 import "./style.css";
-
+import { useCardListStore } from './Store/store.js';
 import AddCard from "./Components/AddCard.jsx";
 import CardList from './Components/CardList.jsx';
 
 function App() 
 {
+const { updateCardList} = useCardListStore();
     async function getCards()
     {
         try{
             const resp = await fetch("http://localhost:5000/getFlashCards");
         const data = await resp.json();
         console.log(data);
+        updateCardList(data);
         }
         catch(err)
         {
             console.log(err);
         }
     }
-    
-    getCards();
+
+    useEffect(()=>
+    {
+        getCards();
+    },[]);
     return (
         <>
             <div className='container'>
